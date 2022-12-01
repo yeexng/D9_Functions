7b4835bb1a27abac72039f1719a5085fcc9de803
yeexng <118996751+yeexng@users.noreply.github.com>
2022-12-01 15:58:19 +0000

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -16,7 +16,7 @@ function area(l1,l2) {
     return l1 * l2
 }
 
-console.log (area(3,3))
+console.log(area(3,3))
 
 /* EXERCISE 2
  Write a function called "crazySum" which receives two integers as parameters.
@@ -34,8 +34,8 @@ function crazysum(n1,n2) {
     
 }
 
-console.log(crazysum(2,3))
-console.log(crazysum(2,2))
+crazysum(2,3)
+crazysum(2,2)
 
 /* EXERCISE 3
  Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
@@ -50,8 +50,8 @@ function crazyDiff(n1) {
     }
 }
 
-console.log(crazyDiff(29))
-console.log(crazyDiff(3))
+crazyDiff(29)
+crazyDiff(3)
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
@@ -59,66 +59,109 @@ console.log(crazyDiff(3))
 
 function boundary(n) {
 
-    if (n => 20) {
-        return true ;
-    } else if (n >= 100) {
+    if (n < 20) {
         return false ;
-    } else if (n === 400){
+    } else if ((n <= 100) || (n === 400)) {
         return true ;
     } else {
       return false ;
     }
     }
     
-    console.log(boundary(15))
-    console.log(boundary(20))
-    console.log(boundary(99))
-    console.log(boundary(401))
-    console.log(boundary(800))
+    boundary(15)
+    boundary(20)
+    boundary(100)
+    boundary(400)
+    boundary(401)
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-function strivify(string) {
-    if (string) {
-        return "Strive" + String
+/* function strivify(str) {
+    let x = "Strive"
+    if (x + str === "strive") {
+        return str
     } else {
-        
+        return "Strive" + str
     }
 }
+
+strivify("words") */
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
 
-/* WRITE YOUR ANSWER HERE */
+function check3and7(n) {
+    let remainderOf3 = n % 3;
+    let remainderOf7 = n % 7;
+
+    if ((remainderOf3 === 0) || (remainderOf7 === 0)) {
+        return "is multiple of 3 or 7"
+    } else {
+        return "is not a multiple of  3 nor 7"
+    }
+
+}
+
+check3and7(9)
+check3and7(49)
+check3and7(100)
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR ANSWER HERE */
+function reverseString(String) {
+    let newString = "";
+    for (let i = String.length - 1; i >= 0; i--){
+        newString += String[i];
+    }
+    return newString;
+}
+
+let result = reverseString("PneumonouPneumonoultramicroscopicsilicovolcanoconiosis") ;
+console.log(result)
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+function upperFirst(string) {
+    let words = string.split("");
+
+    for (i=0; i<words.length; i++){
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+    return words.join("");
+}
+
+console.log(upperFirst("No idea what am i doing"))
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+function cutString(str) {
+
+    let withoutFirstandLast = str.substring(1,str.length -1);
+
+    console.log(withoutFirstandLast);
+    
+}
+
+cutString("thisissoharddddd")
 
 /* EXERCISE 10
  Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
-/* WRITE YOUR ANSWER HERE */
+/* function giveMeRandom(n) {
+    
+} */
 
 /* WHEN YOU ARE FINISHED
  Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
